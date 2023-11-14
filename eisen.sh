eae13051a847ea00dc306c953c88729d54923716
athraz <athrazan2004@gmail.com>
2023-11-14 23:19:58 +0700

    hq turu

--- eisen.sh
+++ eisen.sh
@@ -7,6 +7,9 @@ upstream backend  {
     server 192.213.3.1 weight=640;
     server 192.213.3.2 weight=200;
     server 192.213.3.3 weight=25;
+    # server 192.213.3.1;
+    # server 192.213.3.2;
+    # server 192.213.3.3;
 }
 
 server {
@@ -18,6 +21,13 @@ server {
         proxy_set_header    X-Real-IP $remote_addr;
         proxy_set_header    X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header    Host $http_host;
+
+        auth_basic "Admin";
+        auth_basic_user_file /etc/nginx/rahasiakita/.htpasswd;
+    }
+
+    location ~ /\.ht {
+        deny all;
     }
 
     error_log /var/log/nginx/lb_error.log;
@@ -28,4 +38,9 @@ server {
 ln -s /etc/nginx/sites-available/lb-php /etc/nginx/sites-enabled
 unlink /etc/nginx/sites-enabled/default
 service nginx restart
+
+apt-get install apache2-utils -y
+mkdir /etc/nginx/rahasisakita/
+htpasswd -bc /etc/nginx/rahasisakita/.htpasswd netics ajkE14
+
 echo nameserver 192.213.1.2 > /etc/resolv.conf
