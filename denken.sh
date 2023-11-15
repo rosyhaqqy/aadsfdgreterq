f3e70e2389e4c7587a2660b384d59476ecddabc9
rosyhaqqy <111132143+rosyhaqqy@users.noreply.github.com>
2023-11-15 09:25:15 +0000

    push 13 14

--- denken.sh
+++ denken.sh
@@ -1,1 +1,22 @@
-echo nameserver 192.168.122.1 > /etc/resolv.conf
+apt-get update && apt-get install mariadb-server -y
+service mysql start
+echo '#!/bin/bash
+
+# MySQL connection parameters
+MYSQL_USER="root"
+MYSQL_PASSWORD=""
+# MySQL commands
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD <<EOF
+CREATE USER '\''kelompokE14'\''@'\''%'\'' IDENTIFIED BY '\''passwordE14'\''; 
+CREATE USER '\''kelompokE14'\''@'\''localhost'\'' IDENTIFIED BY '\''passwordE14'\''; 
+CREATE DATABASE dbkelompokE14; 
+GRANT ALL PRIVILEGES ON *.* TO '\''kelompokE14'\''@'\''%'\''; 
+GRANT ALL PRIVILEGES ON *.* TO '\''kelompokE14'\''@'\''localhost'\''; 
+FLUSH PRIVILEGES; 
+EOF' > /run.sh
+chmod +x /run.sh
+./run.sh
+
+echo '[mysqld]
+skip-networking=0
+skip-bind-address' >> /etc/mysql/my.cnf
